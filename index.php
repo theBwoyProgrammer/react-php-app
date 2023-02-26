0b56d48fe0d1536e18a6cbce23eef42fcbb87911
Codeaddictjaver <102610715+HatimDev-HE@users.noreply.github.com>
2023-02-26 21:37:59 +0300

    PHP set httpie

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
-var_dump($_SERVER['REQUEST_URI']) ;
+$parts = explode('/', $_SERVER['REQUEST_URI']) ;
+
+if ($parts[1] != "products") {
+    http_response_code(404);
+    exit;
+}
+
+$id = $parts[2] ?? null;
+
+var_dump($id);
